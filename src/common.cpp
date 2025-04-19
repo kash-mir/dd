cb0e16295ab94457746356ab6d7cc1de9292923f
kash-mir <wiabjtiidsam@gmail.com>
2025-04-19 05:26:47 +0000

    added folders

---
+++ src/common.cpp
@@ -0,0 +1,12 @@
+#include "../inc/common.h"
+
+// Definitions of global pointers
+deviceGraphPointers deviceGraph;
+deviceDAGpointer deviceDAG;
+cliqueLevelDataPointer levelData;
+deviceCliquesPointer cliqueData;
+densestCorePointer  densestCore;
+deviceComponentPointers conComp;
+devicePrunedNeighbors prunedNeighbors;
+deviceCliquesPointer finalCliqueData;
+
